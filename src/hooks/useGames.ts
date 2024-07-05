a514b37c489381407a1167845f87ae96ba245e42
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-05 19:12:37 +0800

    Building game card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import axios, { AxiosError } from 'axios';
 import apiClient from '../services/api-client.ts';
 
 
-interface Game {
+export interface Game {
 	id: number;
 	name: string;
+	background_image: string;
 }
 
 interface FetchGamesResponse {
